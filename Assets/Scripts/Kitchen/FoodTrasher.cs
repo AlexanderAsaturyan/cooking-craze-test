ca25087cf15b34ddc6a6051db4d769616428d0c0
AlexanderAsaturyan <alexanderasaturyan@gmail.com>
2024-01-20 05:17:56 +0400

    Implemented TryTrashFood method.

--- Assets/Scripts/Kitchen/FoodTrasher.cs
+++ Assets/Scripts/Kitchen/FoodTrasher.cs
@@ -1,12 +1,12 @@
 using System;
-
 using UnityEngine;
-
-using JetBrains.Annotations;
+using UnityEngine.EventSystems;
 
 namespace CookingPrototype.Kitchen {
 	[RequireComponent(typeof(FoodPlace))]
-	public sealed class FoodTrasher : MonoBehaviour {
+	public sealed class FoodTrasher : MonoBehaviour, IPointerClickHandler
+		{
+		private Action _onDoubleClicked;	
 
 		FoodPlace _place = null;
 		float     _timer = 0f;
@@ -14,14 +14,26 @@ namespace CookingPrototype.Kitchen {
 		void Start() {
 			_place = GetComponent<FoodPlace>();
 			_timer = Time.realtimeSinceStartup;
+			_onDoubleClicked += TryTrashFood;
 		}
 
 		/// <summary>
 		/// Освобождает место по двойному тапу если еда на этом месте сгоревшая.
 		/// </summary>
-		[UsedImplicitly]
-		public void TryTrashFood() {
-			throw new NotImplementedException("TryTrashFood: this feature is not implemented");
+		public void TryTrashFood()
+		{
+			if( _place.CurFood?.CurStatus == Food.FoodStatus.Overcooked )
+			{
+				_place.FreePlace();
+			}
+		}
+
+		public void OnPointerClick(PointerEventData eventData)
+		{
+			if ( eventData.clickCount == 2 )
+			{
+				_onDoubleClicked?.Invoke();
+			}
 		}
 	}
 }
